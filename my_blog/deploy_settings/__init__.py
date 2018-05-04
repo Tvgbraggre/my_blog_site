b5002af987302f6c35a86464fc00a9a870f0b33e
Tvgbraggre <timothyvgrr@gmail.com>
2018-05-04 14:59:41 +0200

    settings

--- my_blog/deploy_settings/__init__.py
+++ my_blog/deploy_settings/__init__.py
@@ -14,7 +14,5 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 
-db_from_env = dj_database_url.config(
-    default='mysql://Tvgbraggre:TTstlyes04#+@Tvgbraggre.mysql.pythonanywhere-services.com/Tvgbraggre$my_blog'
-)
+db_from_env = dj_database_url.config()
 DATABASES["default"].update(db_from_env)
