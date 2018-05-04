cb08799e234ad45de0d15bedd1292e532a487451
Tvgbraggre <timothyvgrr@gmail.com>
2018-05-04 15:13:34 +0200

    settings

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
         'PASSWORD': 'TTstlyes05#+',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
